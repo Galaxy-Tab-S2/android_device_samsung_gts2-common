ratmole/android_device_samsung_trelte-common

trelte-common: override apns/spn configs from samsung
Imported from heroxlte

device-common.mk
@@ -19,9 +19,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# APN Definitions - override incomplete/broken lineageos version with Google master version
+# APN Definitions - override incomplete/broken lineageos version with Samsung version
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/telephony/apns-full-conf.xml:system/etc/apns-conf.xml
+    $(COMMON_PATH)/configs/telephony/apns-conf.xml:system/etc/apns-conf.xml \
+    $(COMMON_PATH)/configs/telephony/spn-conf.xml:system/etc/spn-conf.xml
 
 
 # ANT+

Galaxy-Tab-S2/android_device_samsung_gts2-common

device-common.mk
@@ -2,9 +2,10 @@ LOCAL_PATH := device/samsung/gts2-common
 
 PRODUCT_CHARACTERISTICS := tablet
 
-# APN Definitions - override incomplete/broken lineageos version with Google master version
+# APN Definitions - override incomplete/broken lineageos version with Samsung version
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/telephony/apns-full-conf.xml:system/etc/apns-conf.xml
+    $(LOCAL_PATH)/configs/telephony/apns-conf.xml:system/etc/apns-conf.xml \
+    $(LOCAL_PATH)/configs/telephony/spn-conf.xml:system/etc/spn-conf.xml
 
 # ANT+
 PRODUCT_PACKAGES += \