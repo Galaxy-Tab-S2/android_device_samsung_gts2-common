ratmole/android_device_samsung_trelte-common

trelte-common: Add samsung swap

device-common.mk
@@ -291,7 +291,8 @@ PRODUCT_PACKAGES += \
     init.universal5433.usb.rc \
     init.samsung.rc \
     init.wifi.rc \
-    init.power.rc
+    init.power.rc \
+    sswap
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \

Galaxy-Tab-S2/android_device_samsung_gts2-common

device-common.mk
@@ -253,7 +253,8 @@ PRODUCT_PACKAGES += \
     init.universal5433.rc \
     init.universal5433.usb.rc \
     init.samsung.rc \
-    init.wifi.rc 
+    init.wifi.rc \
+    sswap
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \